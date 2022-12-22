94f8500f8092b90ae04d31423dfe63e8c478ac02
DinisEstrada <dinis.estrada37@gmail.com>
2022-12-22 15:18:07 +0000

    inicio Fase3

---
+++ Fase3/DB/CreateDB.java
@@ -0,0 +1,25 @@
+package org.example.databaseScripts;
+
+import org.example.data.DatabaseData;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class CreateDatabase {
+
+    public static void main(String[] args) {
+        // Open a connection
+        try(Connection conn = DatabaseData.getConnectionNoDatabase();
+            Statement stmt = conn.createStatement();
+        ) {
+            String sql = "CREATE DATABASE IF NOT EXISTS "+DatabaseData.getDatabaseName()+";";
+            int result = stmt.executeUpdate(sql);
+            if (result == 1) {
+                System.out.println("Database created successfully...");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
