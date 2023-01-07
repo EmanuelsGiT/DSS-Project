7b0241c4a39c305858cfbf02e4065071640887a9
Gonçalo Melo <73468436+goncalo-melo@users.noreply.github.com>
2023-01-07 19:34:10 +0000

    mais alguns

--- Fase3/src/Views/UtilizadorView.java
+++ Fase3/src/Views/UtilizadorView.java
@@ -10,5 +10,17 @@ public class UtilizadorView {
     public void autenticarSucesso(Utilizador u) {
         System.out.println("Utilizador " + u.getNome() + " autenticado com sucesso!");
     }
+    
+    public void utilizadorJaRegistado() {
+       System.out.println("Este utilizador já se encontra registado!");
+    }
+
+    public void sucessoAutenticacao() {
+        System.out.println("Autenticado com sucesso!");
+    }
+
+    public void insucessoAutenticacao() {
+        System.out.println("Erro ao autenticar!");
+    }
 
 }
